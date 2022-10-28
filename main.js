2e45bac3d65097aa75e0ca334cc5ae0120c86098
AndersonNewton <83888063+AndersonNewton@users.noreply.github.com>
2022-10-28 17:47:26 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function preload() {
+
+
+
+}
+
+function setup() {
+
+    canvas = createCanvas(500,650);
+    background("white");
+    canvas.position(700,200);
+
+}
+
+function draw() {
+
+
+
+}
